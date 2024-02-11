b6c531982751a82f21f08f824c4863b3612b2238
mrshaikat <mrshaikat0.cse@gmail.com>
2024-02-11 20:07:08 +0600

    Add weather hook

---
+++ src/components/header/Favourite.jsx
@@ -0,0 +1,10 @@
+import FavouriteLogo from "../../assets/heart.svg";
+
+export default function Favourite() {
+  return (
+    <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
+      <img src={FavouriteLogo} alt="Favourite logo" />
+      <span>Favourite Locations</span>
+    </div>
+  );
+}
